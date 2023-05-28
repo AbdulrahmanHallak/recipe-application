62d173ab1a9bd90bf025b16d22e1092287c7634d
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-05-28 17:50:17 +0300

    Completed the currency converter app

---
+++ CurrencyConverter/CurrencyConverter/CurrencyCodeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurrencyConverter;
+
+public class CurrencyCodeAttribute : ValidationAttribute
+{
+    public string[] _acceptedCodes;
+    public CurrencyCodeAttribute(params string[] acceptedCodes)
+    {
+        _acceptedCodes = acceptedCodes;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is string code && !(_acceptedCodes.Contains(code)))
+        {
+            return new ValidationResult("Not allowed currency code");
+        }
+        return ValidationResult.Success;
+    }
+}
