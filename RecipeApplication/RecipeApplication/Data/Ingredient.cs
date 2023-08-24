beda1f417a39c62bf18fe2c6f71155c2a24ee268
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-08-24 22:43:20 +0300

    Introduce a foreign key property to properly express the relation between entitis
    Define the RecipeId property in Ingredient entity as a foreign key to the recipe each
    ingredient belongs to, which demonstrates the correct perception of domain logic.

--- RecipeApplication/RecipeApplication/Data/Ingredient.cs
+++ RecipeApplication/RecipeApplication/Data/Ingredient.cs
@@ -3,6 +3,7 @@
 public class Ingredient
 {
     public int Id { get; set; }
+    public int RecipeId { get; set; }
     public string Name { get; set; } = string.Empty;
     public decimal Quantity { get; set; }
     public string Unit { get; set; } = string.Empty;
