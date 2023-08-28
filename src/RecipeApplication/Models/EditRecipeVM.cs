6b278e44fccd16069cb8427cf046c335461e06ae
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-08-28 19:51:21 +0300

    Include the entity Id in the view model

--- src/RecipeApplication/Models/EditRecipeVM.cs
+++ src/RecipeApplication/Models/EditRecipeVM.cs
@@ -4,6 +4,8 @@ namespace RecipeApplication.Models.ViewModels;
 
 public class EditRecipeVM
 {
+    public int Id { get; set; }
+    
     [Required, StringLength(50)]
     public string Name { get; set; } = string.Empty;
 
