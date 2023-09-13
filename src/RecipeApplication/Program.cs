50e98f39411f74bdabfbf8171a7b3860caf97e72
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-09-13 21:54:16 +0300

    Add comment

--- src/RecipeApplication/Program.cs
+++ src/RecipeApplication/Program.cs
@@ -8,12 +8,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // TODO: Introduce a custom exception for RecipeNotFound.
         var builder = WebApplication.CreateBuilder(args);
         // Add services to the container.
         builder.Services.AddDbContext<RecipeApplicationContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("RecipeApplicationContext") ?? throw new InvalidOperationException("Connection string 'RecipeApplication' not found.")));
 
+        // Add ASP.NET Core Identity and Google external login.
         builder.Services.AddAuthenticationServices(builder.Configuration);
 
         builder.Services.AddRazorPages();
