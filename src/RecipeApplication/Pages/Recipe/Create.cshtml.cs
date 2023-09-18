7970d1ab6953d6d5625df77a32448c1c167bbf57
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-09-18 11:06:47 +0300

    Add declarative Authorization. Only authenticated users can Create

--- src/RecipeApplication/Pages/Recipe/Create.cshtml.cs
+++ src/RecipeApplication/Pages/Recipe/Create.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RecipeApplication.Models;
 
 namespace RecipeApplication.Pages.Recipe;
 
+[Authorize]
 public class CreateModel : PageModel
 {
     [BindProperty]
