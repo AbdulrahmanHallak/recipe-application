dfe404ea137c299862d0b2d58690d75c0976d620
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-09-19 21:04:30 +0300

    Hide UI elements based on authorization

--- src/RecipeApplication/Pages/Recipe/View.cshtml.cs
+++ src/RecipeApplication/Pages/Recipe/View.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RecipeApplication.Models;
@@ -7,15 +8,23 @@ namespace RecipeApplication.Pages.Recipe;
 public class ViewModel : PageModel
 {
     public RecipeDetailsVM? Recipe { get; set; }
+
+    public bool CanManageRecipe { get; set; } // This is only used to hide UI elements
     private readonly RecipeService _service;
-    public ViewModel(RecipeService service)
+    private readonly IAuthorizationService _authService;
+
+    public ViewModel(RecipeService service, IAuthorizationService authService)
     {
         _service = service;
+        _authService = authService;
     }
     public async Task<IActionResult> OnGetAsync(int id)
     {
-        Recipe = await _service.GetRecipeDetails(id)!;
-        if (Recipe == null) return NotFound();
+        Recipe = await _service.GetRecipeDetails(id)!; // The exception is handled by the ExceptionHandler middleware.
+
+        var recipe = _service.GetRecipeAsync(id);
+        var authResult = await _authService.AuthorizeAsync(User, recipe , "CanManageRecipe");
+        CanManageRecipe = authResult.Succeeded;
 
         return Page();
     }
